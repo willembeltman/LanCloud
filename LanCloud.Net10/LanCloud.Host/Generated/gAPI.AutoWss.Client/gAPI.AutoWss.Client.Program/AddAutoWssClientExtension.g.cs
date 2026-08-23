d80639afe208642977d12aaf51122294dd52627f
Willem Beltman <hwj.beltman@gmail.com>
2026-08-23 22:21:34 +0200

    gAPI update

--- LanCloud.Net10/LanCloud.Host/Generated/gAPI.AutoWss.Client/gAPI.AutoWss.Client.Program/AddAutoWssClientExtension.g.cs
+++ LanCloud.Net10/LanCloud.Host/Generated/gAPI.AutoWss.Client/gAPI.AutoWss.Client.Program/AddAutoWssClientExtension.g.cs
@@ -28,7 +28,7 @@ public static class AddAutoWssClientExtension
 
         // Connection stuff
         //services.TryAddScoped<IClientAuthenticatedHttpClient, EmptyClientAuthenticatedHttpClient>();
-        services.AddAuthenticationServices(apiAddress);
+        //services.AddAuthenticationServices(apiAddress);
         services.AddScoped<ClientConnection>();
         services.AddScoped<IClientConnection>(sp => sp.GetRequiredService<ClientConnection>());
         services.AddScoped<IWssLoggerFactory>(sp => sp.GetRequiredService<ClientConnection>());
--- LanCloud.Net10/LanCloud.Host/Program.cs
+++ LanCloud.Net10/LanCloud.Host/Program.cs
@@ -1,4 +1,5 @@
-﻿using gAPI.Generated;
+﻿using gAPI.Core.Client;
+using gAPI.Generated;
 using LanCloud.Host.Models;
 using LanCloud.Host.Services;
 using LanCloud.Shared.Models;
@@ -11,6 +12,7 @@ var config = new HostConfig([new LocalShare(
 var builder = Host.CreateApplicationBuilder(args);
 builder.Services.AddHostedService<HostHub>();
 builder.Services.AddAutoWssClient("https://localhost:7087", "wss://127.0.0.1:7087");
+builder.Services.AddAuthenticationServices("https://localhost:7087");
 builder.Services.AddSingleton(config);
 
 var app = builder.Build();
