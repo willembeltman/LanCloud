60bc488d09694657c1721308af58df005aeec9ad
Willem Beltman <hwj.beltman@gmail.com>
2026-08-29 04:20:27 +0200

    Wauw het werkt nu 2 kanten op

---
+++ LanCloud.Net10/LanCloud.Api/Generated/gAPI.AutoAuth.Server/gAPI.AutoAuth.Server.Program/AutoSerializer/StateDtoCreateCopy.g.cs
@@ -0,0 +1,19 @@
+﻿using LanCloud.Shared.Dtos;
+using gAPI.Core.Dtos;
+using System.IO;
+using gAPI.Core.AttributesSerializers;
+using gAPI.Core.Attributes;
+
+namespace gAPI.Generated;
+
+public static class StateDtoCreateCopy
+{
+    [IsCreateCopy]
+    public static StateDto CreateCopy(this StateDto value)
+    {
+        var copy = new StateDto();
+        copy.User = value.User == null ? null : value.User.CreateCopy();
+        copy.ForceReconnect = value.ForceReconnect;
+        return copy;
+    }
+}
